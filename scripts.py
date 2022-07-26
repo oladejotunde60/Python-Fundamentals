b3f3286e98a9251d4416a137c680bb5ced6eca99
Tunde <oladejotunde60@gmail.com>
2022-07-26 21:16:59 +0100

    Compare word

--- scripts.py
+++ scripts.py
@@ -24,3 +24,29 @@ replace_domain('t@ab.com', 'ab.com', 'abc.com')
 #Compare strings
 word = "This is the beginning of my wonderful Python exploit"
 "This" in word
+
+#lower() and upper() change case
+ans = "YES"
+if ans.lower() == "yes":
+    print("User said yes")
+    
+#strip() removes spaces
+g = "  yes  "
+h = g.strip()
+p = g.lstrip()
+y = g.rstrip()
+print(h, p, y)
+
+#"This returns how many times an alphabet retrn".count("e")
+t = "This returns how many times an alphabet retrn".count("e")
+print(t)
+
+#endswith,prints string that ends with a substring
+l = 'forest'.endswith("st")
+print(l)
+
+#isnumeric checks if the strings are just number
+u = "Hello world!".isnumeric()
+print(u)
+r = "234".isnumeric()
+print(r)
