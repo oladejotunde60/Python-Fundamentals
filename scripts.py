93c543befcb0d5e26a83b30ef09741da1a951c56
Tunde <oladejottunde60@gmail.com>
2022-07-26 19:55:07 +0100

    Functoin that replaces the old domain with new domain

--- scripts.py
+++ scripts.py
@@ -9,3 +9,14 @@ def first_and_last(message):
 print(first_and_last("else"))
 print(first_and_last("tree"))
 print(first_and_last(""))
+
+
+#Function that replaces domain of an email
+
+def replace_domain(email, old_domain, new_domain):
+    if "@" + old_domain in email:
+        index = email.index("@" + old_domain)
+        new_email = email[:index] + "@" + new_domain
+        return new_email
+    return email
+replace_domain('t@ab.com', 'ab.com', 'abc.com')
