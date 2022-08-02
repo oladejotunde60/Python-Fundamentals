0411b31552cda2d26f81288798010faea9fc149c
Tunde <oladejottunde60@gmail.com>
2022-08-02 23:17:36 +0100

    Tuple

--- scripts.py
+++ scripts.py
@@ -145,3 +145,15 @@ print(get_word("This is a lesson about lists", -4)) # Nothing
 print(get_word("Now we are cooking!", 1)) # Should print: Now
 print(get_word("Now we are cooking!", 5)) # Nothing
 
+#Tuple example
+def file_size(file_info):
+	name, types, size = file_info
+	return("{:.2f}".format(size / 1024))
+
+print(file_size(('Class Assignment', 'docx', 17875))) # Should print 17.46
+print(file_size(('Notes', 'txt', 496))) # Should print 0.48
+print(file_size(('Program', 'py', 1239))) # Should print 1.21
+
+
+
+
